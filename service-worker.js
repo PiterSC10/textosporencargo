e6d5bb218bb14321b30b514a34d10c98116fd4c1
PiterSC10 <pedrosorianocalderon@gmail.com>
2026-08-18 00:08:32 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'ydg-static-v70-weather-refresh';
+const CACHE_NAME = 'ydg-static-v71-weather-manual';
 const CORE_ASSETS = [
   './index.html',
-  './site.webmanifest?v=70',
+  './site.webmanifest?v=71',
   './android-chrome-192x192.png',
   './android-chrome-512x512.png',
   './apple-touch-icon.png',
